1a3045ac22a23573f4320007fb2bdcabe5d034e9
Manav Kaneria <manav.kaneria@aliansoftware.com>
2024-10-18 18:33:42 +0530

    Reports.jsx updated and Navbar.css height updated

--- src/pages/Reports.jsx
+++ src/pages/Reports.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from "react";
+import { loadFromLocalStorage } from "../utils/localStorageUtil";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  Grid,
+  Container,
+} from "@mui/material";
 
 const Reports = () => {
-  return (
-    <div><h1>Reports</h1></div>
-  )
-}
+  const responses = loadFromLocalStorage("userDetails") || []; // Load user details from local storage
 
-export default Reports
+  return (
+    <Container maxWidth="md:w-10" sx={{ mt: 5 }}>
+      <h1>Reports</h1>
+      <Grid container spacing={2}>
+        {responses.map((user, index) => (
+          <Grid item xs={12} sm={6} key={index}>
+     
+            <Card
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                mb: 2,
+                width: "max-content", // Set width to max-content
+                minWidth: ""
+              }}
+            >
+              <CardMedia
+                component="img"
+                sx={{ width: 150, height: 150, marginLeft: 2 }} // Set dimensions for the image
+                image={user.photo}
+                alt={`${user.name}'s photo`}
+              />
+              <CardContent sx={{ flex: 1 }}>
+                <Typography variant="h6" noWrap>
+                  {user.name}
+                </Typography>
+                <Typography variant="body1" sx={{ wordWrap: "break-word" }}>
+                  Email: {user.email}
+                </Typography>
+                <Typography variant="body1" sx={{ wordWrap: "break-word" }}>
+                  Phone: {user.phone}
+                </Typography>
+                <Typography variant="body1" sx={{ wordWrap: "break-word" }}>
+                  Address: {user.address}
+                </Typography>
+                <Typography variant="body1" sx={{ wordWrap: "break-word" }}>
+                  Feedback: {user.feedback}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+};
+
+export default Reports;
